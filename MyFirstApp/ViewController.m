836f060a5c2f9914d09ae03304d0aab891d2a853
jagio0129 <jagio0129@gmail.com>
2019-05-08 23:47:51 +0900

    Buttonの追加

--- MyFirstApp/ViewController.m
+++ MyFirstApp/ViewController.m
@@ -19,11 +19,22 @@
     
     NSLog(@"viewDidLoad");
     
+    // UILabelの作成
     CGRect rect = [self.view frame];
     UILabel* label = [[UILabel alloc] initWithFrame:rect];
     label.text = @"Hello View World!";
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:label];
+    
+    // ボタンの作成
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button setTitle:@"Touch me!" forState:UIControlStateNormal];
+    [button sizeToFit];
+    button.center = self.view.center;
+    [button addTarget:self
+            action:@selector(buttonDidPush)
+            forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
 
