fb6a7781a8f2619048c85d917e70fd9bce5cbec0
jagio0129 <jagio0129@gmail.com>
2019-05-12 17:41:12 +0900

    rename ViewController to MainViewController

--- MyFirstApp/AppDelegate.m
+++ MyFirstApp/AppDelegate.m
@@ -18,8 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.viewController = [[ViewController alloc] init];
-    self.window.rootViewController = self.viewController;
+    self.mainViewController = [[MainViewController alloc] init];
+    self.window.rootViewController = self.mainViewController;
     [self.window makeKeyAndVisible];
     
     return YES;
